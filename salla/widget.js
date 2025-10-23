a3034aed66ce35c7f4a2c79ae13eef9ebb82a458
mohamedellithy <develomohamed22@gmail.com>
2025-10-23 23:44:05 +0300

    Release v1.2.0

--- salla/widget.js
+++ salla/widget.js
@@ -79,29 +79,30 @@ async function render_whatsapp() {
                 }
             });
         });
+        
+        // close
+        await document.getElementById('wa-close').addEventListener("click", (e) => {
+            document.getElementById('wa-popup').style.display = 'none';
+        });
+    
+        // wa-send
+        await document.getElementById('wa-send').addEventListener("click", (e) => {
+            let message = document.getElementById('wa-message').value.trim();
+            if(!message) {
+                alert('يرجى كتابة رسالة لإرسالها عبر واتساب');
+                return;
+            }
+    
+            var phone = document.getElementById('wa-phone').value;
+            var link  = 'https://wa.me/' + phone + '?text=' + encodeURIComponent(msg);
+            window.open(link, '_blank');
+            document.getElementById('wa-message').value = '';
+            document.getElementById('wa-popup').style.display = 'none';
+        });
     }
 }
+
 
 // here all
 render_whatsapp();
 // here all
-
-// close
-document.getElementById('wa-close').addEventListener("click", (e) => {
-    document.getElementById('wa-popup').style.display = 'none';
-});
-
-// wa-send
-document.getElementById('wa-send').addEventListener("click", (e) => {
-    let message = document.getElementById('wa-message').value.trim();
-    if(!message) {
-        alert('يرجى كتابة رسالة لإرسالها عبر واتساب');
-        return;
-    }
-
-    var phone = document.getElementById('wa-phone').value;
-    var link  = 'https://wa.me/' + phone + '?text=' + encodeURIComponent(msg);
-    window.open(link, '_blank');
-    document.getElementById('wa-message').value = '';
-    document.getElementById('wa-popup').style.display = 'none';
-});
