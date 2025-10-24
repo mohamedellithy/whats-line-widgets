d5d8de0a216dcae21382f31affdf323fdcb0c202
mohamedellithy <develomohamed22@gmail.com>
2025-10-24 11:32:58 +0300

    sd

--- salla/widget.js
+++ salla/widget.js
@@ -1,7 +1,7 @@
 
 async function render_whatsapp() {
     if(whatsappln_icon_status == true){
-        console.log("WhatsAppLn Widget v1.0.4");
+        console.log("WhatsAppLn Widget v1.0.6");
         // Create the <a> element
         const link = document.createElement("a")
         link.href = "#";
